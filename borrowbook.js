7f472e868072b2230457e86cd5b14c3fd79739f4
geni-s <geniusjareda567@gmail.com>
2026-05-27 10:00:00 +0530

    select option in category

--- borrowbook.js
+++ borrowbook.js
@@ -14,8 +14,8 @@ const search=document.getElementById("search");
 
 search.addEventListener("click", () => {
    const author=document.getElementById("text1").value;
-   const book=document.getElementById("text2").value;
-   const category=document.getElementById("text3").value;
+   const book=document.getElementById("text3").value;
+   const category=document.getElementById("text2").value;
    localStorage.setItem("author", author);
    localStorage.setItem("book", book);
    localStorage.setItem("category", category);
